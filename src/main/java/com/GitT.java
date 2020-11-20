c68cb65d114bf382399f6784a7501feacbba7275
lzbglo <lzbglo@qq.com>
2020-11-20 20:37:34 +0800

    this is clone

--- src/main/java/com/GitT.java
+++ src/main/java/com/GitT.java
@@ -4,6 +4,7 @@ public class GitT {
     public static void main(String[] args) {
         System.out.println("love you");
         System.out.println("edit");
+        System.out.println("clone");
     }
 
 }
